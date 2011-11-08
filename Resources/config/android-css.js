398e2ee2bf79e526deeaae955bd5a88f76997636
Low Kian Seong <kianseong@gmail.com>
2011-11-08 14:54:03 +0800

    Add iphone deployment

---
+++ Resources/config/android-css.js
@@ -0,0 +1,60 @@
+
+
+(function() {
+	jenkins.ui.css = {
+			titleBar: {
+				top: 0,
+				height: 30,
+				backgroundColor: "#000"
+			},
+			actionButtonBar: {
+				backgroundColor: "#000",
+				height : 60,
+				top: 30
+			},
+			actionButton: {
+				height: 50,
+				width: 50,
+				top: 5,
+			},
+			listView: {
+				top: 90
+			},
+			settingNewPopup : { 
+				top: 130,
+				left: 10,
+				right: 10,
+				height: 250,
+				backgroundColor: "#424542",
+				borderRadius: 2,
+				borderWidth: 1,
+				borderColor: "#bdbebd",
+				visible: false
+			},
+			setttingNewPopupInput: {
+				color: "#336699",
+				left: 10,
+				right: 10,
+				height: 60,
+				keyboardType: Titanium.UI.KEYBOARD_DEFAULT,
+				returnKeyType: Titanium.UI.RETURNKEY_DEFAULT,
+				borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+			},
+			setttingNewPopupButton: {
+				top: 200,
+				width: 90,
+				height: 45,
+				borderRadius:1,
+				color: "#666464",
+				font: {fontFamily: "Arial", fontWeight: "bold", fontSize: 11}
+			},
+			setttingNewPopupTitle: { 
+				height: 40,
+				left: 10,
+				color: "#fff",
+				font: { fontSize: 14 } 
+			}
+	};
+})();
+
+
---
+++ Resources/config/iphone-css.js
@@ -0,0 +1,60 @@
+
+
+(function() {
+	jenkins.ui.css = {
+			titleBar: {
+				top: 0,
+				height: 30,
+				backgroundColor: "#000"
+			},
+			actionButtonBar: {
+				backgroundColor: "#000",
+				height : 60,
+				top: 30
+			},
+			actionButton: {
+				height: 50,
+				width: 50,
+				top: 5,
+			},
+			listView: {
+				top: 90
+			},
+			settingNewPopup : { 
+				top: 60,
+				left: 10,
+				right: 10,
+				height: 215,
+				backgroundColor: "#424542",
+				borderRadius: 2,
+				borderWidth: 1,
+				borderColor: "#bdbebd",
+				visible: false
+			},
+			setttingNewPopupInput: {
+				color: "#336699",
+				left: 10,
+				right: 10,
+				height: 45,
+				keyboardType: Titanium.UI.KEYBOARD_DEFAULT,
+				returnKeyType: Titanium.UI.RETURNKEY_DEFAULT,
+				borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+			},
+			setttingNewPopupButton: {
+				top: 160,
+				width: 60,
+				height: 35,
+				borderRadius:1,
+				color: "#666464",
+				font: {fontFamily: "Arial", fontWeight: "bold", fontSize: 11}
+			},
+			setttingNewPopupTitle: { 
+				height: 40,
+				left: 10,
+				color: "#fff",
+				font: { fontSize: 14 } 
+			}
+	};
+})();
+
+
